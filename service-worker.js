bf7f517041f0bc64503278622427970c578be11a
Rashishreshu <155653198+Rashishreshu@users.noreply.github.com>
2025-09-03 20:48:35 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "todo-list-v1";
+const urlsToCache = [
+    "/index.html",
+    "/manifest.json",
+    "/images/icon-192.png",
+    "/images/icon-512.png"
+];
+
+// Install service worker
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Clean up old caches
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                    .filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            );
+        })
+    );
+});
+
+// Fetch cached files when offline
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
